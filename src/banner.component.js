32cbd46b3fb32dd941f5425a001238f3766f9ce6
hritikchokker <hchokker7@gmail.com>
2021-04-28 15:06:26 +0530

    minor changes

---
+++ src/banner.component.js
@@ -0,0 +1,11 @@
+import tenor from './tenor.gif';
+export default function BannerComponent() {
+    return (
+        <div>
+            <h1>
+                Moj Kradi Bete
+            </h1>
+            <img height="300px" src={tenor} alt="logo" />
+        </div>
+    )
+}
